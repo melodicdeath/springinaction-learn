17fbfecbd122619522646ed8dae592a44fbd5520
zt.melody <zt.freedom@gmail.com>
2017-11-07 01:41:20 +0800

    中文乱码及json转换器问题

--- Chapter_05/src/main/java/org/melodicdeath/api/ApiController.java
+++ Chapter_05/src/main/java/org/melodicdeath/api/ApiController.java
@@ -1,20 +1,53 @@
 package org.melodicdeath.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/api")
+@Validated
 public class ApiController {
 
     @GetMapping("/say/{name}")
-//    @RequestMapping(value = "/say/{name}", method = RequestMethod.GET)
-//    @ResponseBody
-    public String say(@PathVariable @Size(min = 1, max = 5) String name) {
-        return name;
+    public @NotNull(message = "返回值不能为空")
+    Message say(@PathVariable @Size(min = 1, max = 2,message = "长度必须为两个字符") String name) {
+        if (name.length() == 1) {
+            return null;
+        } else {
+            return new Message("草泥马");
+        }
+    }
+
+    public class Message{
+        String msg;
+
+        Message(String msg) {
+            this.msg = msg;
+        }
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleValidationFailure(ConstraintViolationException ex) throws UnsupportedEncodingException {
+
+        StringBuilder messages = new StringBuilder();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            messages.append(violation.getMessage() + "\n");
+        }
+
+        return messages.toString();
     }
 }
