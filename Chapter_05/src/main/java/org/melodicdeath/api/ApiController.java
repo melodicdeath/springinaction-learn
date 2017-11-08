9c25114214547662e1d04c2c70d9db9311418a6d
zt.melody <zt.freedom@gmail.com>
2017-11-09 01:44:13 +0800

    swagger测试

--- Chapter_05/src/main/java/org/melodicdeath/api/ApiController.java
+++ Chapter_05/src/main/java/org/melodicdeath/api/ApiController.java
@@ -1,11 +1,10 @@
 package org.melodicdeath.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,25 +19,56 @@ import java.io.UnsupportedEncodingException;
 @RestController
 @RequestMapping("/api")
 @Validated
-@Api(value = "测试api")
+@Api(value = "测试api", tags = "tags1",produces = MediaType.APPLICATION_JSON_UTF8_VALUE,description = "desc")
 public class ApiController {
 
     @ApiOperation(value = "test", notes = "notes")
-    @ApiImplicitParam(name = "name", value = "姓名", required = true, dataType = "String")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "姓名", required = true, dataType = "String", paramType = "path"),
+            @ApiImplicitParam(name = "token", value = "token", required = true, dataType = "String", paramType = "header")
+    })
     @GetMapping("/say/{name}")
     public @NotNull(message = "返回值不能为空")
-    Message say(@PathVariable @Size(min = 1, max = 2,message = "长度必须为两个字符") String name) {
+    String say(@PathVariable @Size(min = 1, max = 2, message = "长度必须为两个字符") String name) {
         if (name.length() == 1) {
             return null;
         } else {
-            return new Message("草泥马");
+            return "草泥马";
         }
     }
 
-    public class Message{
-        String msg;
+    @ApiOperation(value = "test json")
+    @PostMapping(value = "/say2", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Message say2(@RequestBody Message message) {
+        return message;
+    }
 
-        Message(String msg) {
+    @ApiOperation(value = "test formurlencoded")
+    @PostMapping(value = "/say3", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public Message say3(@RequestBody @ModelAttribute Message message) {
+        return message;
+    }
+
+    @ApiModel(description = "消息对象消息对象消息对象")
+    static class Message {
+        @ApiModelProperty(value = "id", required = true)
+        private int code;
+        @ApiModelProperty(value = "消息", required = true)
+        private String msg;
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
             this.msg = msg;
         }
     }
--- Chapter_05/src/main/java/org/melodicdeath/config/SwaggerConfig.java
+++ Chapter_05/src/main/java/org/melodicdeath/config/SwaggerConfig.java
@@ -6,13 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.common.base.Predicates.not;
 import static com.google.common.base.Predicates.or;
@@ -32,7 +38,22 @@ public class SwaggerConfig {
                 .apis(withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
                 .build();
+//                .globalOperationParameters(setHeaderToken());
     }
+
+    @Bean
+    public Docket createRestApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("test api2")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.melodicdeath"))
+                .apis(withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+//                .globalOperationParameters(setHeaderToken());
+    }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
@@ -43,4 +64,11 @@ public class SwaggerConfig {
                 .build();
     }
 
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("X-Auth-Token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
 }
